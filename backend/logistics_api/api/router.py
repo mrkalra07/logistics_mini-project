ce651224da8b922026d6cb21e96819159dadc658
Lakshay Kalra <157738849+mrkalra07@users.noreply.github.com>
2026-03-29 23:35:06 +0530

    Delete backend/logistics_api directory

--- backend/logistics_api/api/router.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from controllers import shipment_controller
-
-router = APIRouter()
-
-router.include_router(shipment_controller.router)
--- backend/logistics_api/models/shipment_model.py
+++
@@ -1,8 +0,0 @@
-def create_shipment_model(data, tracking_number):
-    return {
-        "tracking_number": tracking_number,
-        "customer_name": data.get("customer_name"),
-        "source": data.get("source"),
-        "destination": data.get("destination"),
-        "status": "created"
-    }
